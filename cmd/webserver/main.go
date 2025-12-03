2132502e1b2f2d032a7eb75bc65b7627690bf44b
Samurosa <75kno15co10s@gmail.com>
2025-12-04 00:03:05 +0300

    feature: init config

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,6 +1,30 @@
-package webserver
+package main
+
+import (
+	"fmt"
+	"log/slog"
+
+	"github.com/spf13/pflag"
+
+	"AvailabilityLinks/internal/config"
+)
 
 func main() {
+
+	configPath := pafseFlags()
+
+	cfg, err := config.MustLoadConfig(configPath)
+	if err != nil {
+		slog.Error("Error loading config", slog.Any("error", err))
+	}
+
+	if cfg == nil {
+		slog.Error("Failed to load config")
+		return
+	}
+
+	fmt.Println(cfg)
+
 	//TODO: init config: cleanenv
 
 	//TODO: init logger: log/slog
@@ -8,4 +32,13 @@ func main() {
 	//TODO: init storage: sqlite
 
 	//TODO: init route: chi, "chi render"
+
+	//TODO: start server
+}
+
+func pafseFlags() string {
+	var configPath string
+	pflag.StringVar(&configPath, "config", "configs/local.yaml", "Path to config file")
+	pflag.Parse()
+	return configPath
 }
